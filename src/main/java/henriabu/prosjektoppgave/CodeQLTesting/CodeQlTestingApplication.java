81f39daf719c33aca33eb19e39c9bbea7025926e
henrikburmann <haburmann@gmail.com>
2025-02-06 10:00:42 +0100

    First commit

---
+++ src/main/java/henriabu/prosjektoppgave/CodeQLTesting/CodeQlTestingApplication.java
@@ -0,0 +1,13 @@
+package henriabu.prosjektoppgave.CodeQLTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeQlTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeQlTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/henriabu/prosjektoppgave/CodeQLTesting/CodeQlTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package henriabu.prosjektoppgave.CodeQLTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQlTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
